66093fa2b9225290d6ae67b089fa3217ba3715a1
Alexandre Hembert <alexandrehembert@leni-147-69.irsf>
2018-12-19 15:37:55 +0100

    pannel admin droit etc

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,20 +11,61 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 class UserType extends AbstractType {
+
+
+    public function __construct(AuthorizationCheckerInterface $securityChecker, TokenStorageInterface $token)
+    {
+        $this->securityChecker = $securityChecker;
+        $this->token = $token;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('email', EmailType::class)
             ->add('nom', TextType::class)
             ->add('prenom', TextType::class)
-            ->add('roles', RolesType::class)
+
             ->add('plainPassword', RepeatedType::class, array(
                     'type' => PasswordType::class,
                     'first_options' => array('label' => 'Password'),
                     'second_options' => array('label' => 'Repeat Password'),
                 )
             );
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+
+        $form = $event->getForm();
+        $user = $event->getData();
+
+        /* @Explain Si Role Super ADMIN alors on ajoute un champ active sinon on enleve le role
+         * if($this->securityChecker->isGranted('ROLE_SUPER_ADMIN') === true){
+         * $form->add('enabled');
+         * }else{
+         * $form->remove('roles');
+         * }
+         */
+        if($this->securityChecker->isGranted('ROLE_ADMIN') === true){
+            $form ->add('roles', RolesType::class);
+
+        }
+
+
+
+
+
+
     }
 
     public function configureOptions(OptionsResolver $resolver) {
