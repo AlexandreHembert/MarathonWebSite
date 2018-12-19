fe23d5005c91ca0a8b7708dbe3cfdda72ce8b4c0
giovanni_loope <giovanni_loope@ens.univ-artois.fr>
2018-12-19 11:49:41 +0100

    ChapType fix

--- src/Form/ChapitreType.php
+++ src/Form/ChapitreType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Chapitre;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,7 @@ class ChapitreType extends AbstractType {
             ->add('titreCourt')
             ->add('photo')
             ->add('question')
-            ->add('premier')
+            ->add('premier',CheckboxType::class)
             ->add('histoire');
     }
 
