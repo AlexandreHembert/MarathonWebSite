cb2666c0784d752368adcbdb6c1dcf4a20391f7d
giovanni_loope <giovanni_loope@ens.univ-artois.fr>
2018-12-19 20:30:46 +0100

    new

--- src/Form/SuiteType.php
+++ src/Form/SuiteType.php
@@ -4,20 +4,44 @@ namespace App\Form;
 
 use App\Entity\Chapitre;
 use App\Entity\Suite;
+use App\Repository\ChapitreRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
 
 class SuiteType extends AbstractType {
+    public function __construct(AuthorizationCheckerInterface $securityChecker, TokenStorageInterface $token)
+    {
+        $this->securityChecker = $securityChecker;
+        $this->token = $token;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $this->chapitres = $options['chapitres'];
 
         $builder
-            ->add('chapitreSource', null, ['data' => $this->chapitres])
+            /*->add('chapitreSource', null, array(
+            'expanded' => false,
+            'multiple' => false,
+            'label' => 'chapitre',
+            'class' => Chapitre::class,
+            'required' => true,
+            'query_builder' => function(ChapitreRepository $er) use($this->token){
+                return $er->findBy(["user" => $this->token->getToken()->getUser()]);
+            }
+        ))*/
             ->add('chapitreDestination')
             ->add('reponse');
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver) {
@@ -31,7 +55,6 @@ class SuiteType extends AbstractType {
     public function preSetData(FormEvent $event) {
         $form = $event->getForm();
         $suite = $event->getData();
-
     }
 }
 
