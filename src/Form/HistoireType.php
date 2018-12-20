61e41933783e7f1099e6ad2237825e251932d035
giovanni_loope <giovanni_loope@ens.univ-artois.fr>
2018-12-20 04:12:31 +0100

    gio

--- src/Form/HistoireType.php
+++ src/Form/HistoireType.php
@@ -7,10 +7,21 @@ use Doctrine\DBAL\Types\BooleanType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class HistoireType extends AbstractType
 {
+
+    public function __construct(AuthorizationCheckerInterface $securityChecker, TokenStorageInterface $token)
+    {
+        $this->securityChecker = $securityChecker;
+        $this->token = $token;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -21,6 +32,10 @@ class HistoireType extends AbstractType
             ->add('genre')
             ->add('user')
         ;
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -29,4 +44,15 @@ class HistoireType extends AbstractType
             'data_class' => Histoire::class,
         ]);
     }
+
+    public function preSetData(FormEvent $event){
+        $form = $event->getForm();
+        $histoire = $event->getData();
+
+        if(!($this->securityChecker->isGranted('ROLE_SUPER_ADMIN') === true)) {
+            $histoire->setUser($this->token->getToken()->getUser());
+            $form->remove("user");
+        }
+    }
+
 }
